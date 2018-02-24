76cd13ea44758e645956da150966bfabc4988570
Shri <shridot215@gmail.com>
2018-02-24 15:17:53 +0000

    Release version 1

---
+++ FirstFile.java
@@ -0,0 +1,8 @@
+public class FirstFile {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("This is my first project on Jenkins");
+    }
+
+}
