74e7d94960dbdd6457a4572df62c757b60444a5c
Shri <shridot215@gmail.com>
2018-02-24 16:18:35 +0000

    Added new project

---
+++ Project1/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+* Unit test for simple App.
+*/
+
+public class AppTest {
+   @Test
+   public void testApp() {
+      App appObject = new App();
+      Assert.assertEquals(appObject.reverseString("Test!"), "!tseT");
+   }
+}
